2f7048ff250a319fac3d70736fc163f66947aed1
Delaney Harwell <delaneyharwell@dhcp-172-24-197-198.mobile.uci.edu>
2025-01-23 16:05:56 -0800

    created tokenize function

--- PartA.py
+++ PartA.py
@@ -2,8 +2,19 @@ import re
 import sys
 from collections import defaultdict
 
+def tokenize(file_path):
+    try:
+        with open(file_path, 'r', encoding='utf-8') as file:
+            text = file.read()
+        tokens = re.findall(r'[a-zA-Z0-9]+', text.lower())
+        return tokens
+    except FileNotFoundError:
+        print(f"Error: file '{file_path}' not found.")
+        return
+
 if __name__ == "__main__":
     if len(sys.argv) != 2:
         print("Must provide a file")
         sys.exit(1)
     text_file_path = sys.argv[1]
+    tokens = tokenize(text_file_path)
